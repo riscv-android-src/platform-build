005cf16dd2f8237766c9c5a4c109d2bb1314619c
McKnight22 <tao.wang.22@outlook.com>
2022-07-25 23:15:21 +0800

    Remove the currently unnecessary targets sdk_phone_riscv64 and sdk_riscv64
    Signed-off-by: McKnight22 <tao.wang.22@outlook.com>

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -65,10 +65,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mainline_system_x86_64.mk \
     $(LOCAL_DIR)/mainline_system_x86_arm.mk \
     $(LOCAL_DIR)/sdk_arm64.mk \
-    $(LOCAL_DIR)/sdk_riscv64.mk \
     $(LOCAL_DIR)/sdk.mk \
     $(LOCAL_DIR)/sdk_phone_arm64.mk \
-    $(LOCAL_DIR)/sdk_phone_riscv64.mk \
     $(LOCAL_DIR)/sdk_phone_armv7.mk \
     $(LOCAL_DIR)/sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/sdk_phone_x86.mk \
