c5d0eb5a51a914a86521729066ff49827b3a404f
McKnight22 <tao.wang.22@outlook.com>
2022-07-25 23:05:43 +0800

    Revert the mistakenly deleted row which includes mainline_system_x86.mk
    Signed-off-by: McKnight22 <tao.wang.22@outlook.com>

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -62,6 +62,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/generic_x86.mk \
     $(LOCAL_DIR)/mainline_system_arm64.mk \
     $(LOCAL_DIR)/mainline_system_riscv64.mk \
+    $(LOCAL_DIR)/mainline_system_x86.mk \
     $(LOCAL_DIR)/mainline_system_x86_64.mk \
     $(LOCAL_DIR)/mainline_system_x86_arm.mk \
     $(LOCAL_DIR)/sdk_arm64.mk \
